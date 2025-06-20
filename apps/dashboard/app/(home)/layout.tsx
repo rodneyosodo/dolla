a2f35c8122dacce05a7f1019874e2ce64994d61b
Rodney Osodo <socials@rodneyosodo.com>
2025-06-20 15:19:18 +0300

    feat(dashboard): add theme toggle and export NavHeader to a component

--- apps/dashboard/app/(home)/layout.tsx
+++ apps/dashboard/app/(home)/layout.tsx
@@ -1,6 +1,7 @@
 import { SidebarProvider } from "@workspace/ui/components/sidebar";
 import { ReactNode } from "react";
 import { AppSidebar } from "@/components/app-sidebar";
+import { ModeToggle } from "@/components/theme-switcher";
 
 export default function RootLayout({
   children,
