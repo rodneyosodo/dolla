0da8eb54d603568ae8d4d966fa19fbc6448c16de
Rodney Osodo <socials@rodneyosodo.com>
2025-06-21 01:23:25 +0300

    feat(dashboard): add base cards for dashbooard UI

--- apps/dashboard/app/(home)/page.tsx
+++ apps/dashboard/app/(home)/page.tsx
@@ -1,17 +1,21 @@
 import { SidebarInset } from "@workspace/ui/components/sidebar";
 import NavHeader from "@/components/nav-header";
+import { ChartAreaInteractive } from "./components/chart-area-interactive";
+import { SectionCards } from "./components/section-cards";
 
 export default function Page() {
   return (
     <SidebarInset>
       <NavHeader parent={{ title: "Dashboard", url: "/" }} />
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="bg-muted/50 aspect-video rounded-xl" />
-          <div className="bg-muted/50 aspect-video rounded-xl" />
-          <div className="bg-muted/50 aspect-video rounded-xl" />
+      <div className="flex flex-1 flex-col">
+        <div className="@container/main flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <SectionCards />
+            <div className="px-4 lg:px-6">
+              <ChartAreaInteractive />
+            </div>
+          </div>
         </div>
-        <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
       </div>
     </SidebarInset>
   );
